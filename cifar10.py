1981e8f8e9c1881750cc39ccd597b24cb6369893
Mark Chevallier <mchevallier@euclid-35.maths.gla.ac.uk>
2017-12-14 11:44:13 +0000

    proper training finished

--- cifar10.py
+++ cifar10.py
@@ -3,6 +3,7 @@ import time,os
 from PIL import Image
 import numpy as np
 from keras.utils.np_utils import to_categorical
+from keras.callbacks import EarlyStopping
 from deepModels import getModel
 from keras.preprocessing.image import ImageDataGenerator
 from sklearn.utils import shuffle
@@ -101,8 +102,8 @@ X_train = np.asarray(X_train)
 y_train = np.asarray(y_train)
 X_test = np.asarray(X_test)
 y_test = np.asarray(y_test)
-X_train = X_train.astype('float32')/255
-X_test = X_test.astype('float32')/255
+#X_train = X_train.astype('float32')/255
+#X_test = X_test.astype('float32')/255
 
 shuffle_index = np.random.permutation(X_train.shape[0])
 X_train = X_train[shuffle_index]
@@ -116,11 +117,13 @@ datagen = ImageDataGenerator(zoom_range=0.2, vertical_flip=True,
                              horizontal_flip=True)
 
 
+callbacks = [EarlyStopping(monitor='val_loss', patience=2, verbose=1)]
+
 # train the model
 start = time.time()
 print('Training starts')
 model_info = model.fit_generator(datagen.flow(X_train, y_train, batch_size = 128),
-                                 steps_per_epoch = 512, nb_epoch = 200, 
+                                 steps_per_epoch = 512, epochs = 200, callbacks=callbacks,
                                  validation_data = (X_test, y_test), verbose=1)
 end = time.time()
 
