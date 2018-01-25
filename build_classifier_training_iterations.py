3b31fc3e43a0d71877785db89789e2b65f9113d0
Mark Chevallier <mchevallier@euclid-35.maths.gla.ac.uk>
2018-01-25 11:39:44 +0000

    iterator mid-debugging, changes to false positives finder

--- build_classifier_training_iterations.py
+++ build_classifier_training_iterations.py
@@ -17,6 +17,7 @@ fcnmodel.load_weights('vgg16-header_classifier.h5')
 df = pd.read_csv('2015-checked-train.txt', header=None)
 df2 = pd.read_csv('2015-Z-LOCATIONS.csv')
 not_wildebeest = pd.DataFrame(columns=['image_name', 'xcoord', 'ycoord'])
+not_wildebeest.to_csv('new_not_wildebeest_locations.csv', header=True, mode='w')
 
 for i in range(df.size):
 	x_pos, y_pos = 0,0
@@ -24,15 +25,20 @@ for i in range(df.size):
 	print('Examining image ' + str(i+1) + ' of ' + str(df.size) +  ': ' + filename)
 	img = Image.open('2015/' + str(filename) + '.JPG')
 	df3 = df2.loc[df2.image_name == filename]
+	imarr = np.zeros((img.size[0], img.size[1], 3))
+	filenames = []
+	x_ts = []
+	y_ts = []
 	for y_pos in range(0, img.size[1], ny):
 		for x_pos in range(0, img.size[0], nx):
 			arr = np.array(img)
 			arr = arr[x_pos:nx+x_pos,y_pos:ny+y_pos,:]
-			if arr.shape[0] < nx:
-				arr2 = np.zeros((nx - arr.shape[0], arr.shape[1], 3))
+			arrx, arry = arr.shape[0], arr.shape[1]
+			if arrx < nx:
+				arr2 = np.zeros((nx - arrx, arry, 3))
 				arr = np.concatenate((arr, arr2), axis=0)
-			if arr.shape[1] < ny:
-				arr2 = np.zeros((arr.shape[0], ny - arr.shape[1], 3))
+			if arry < ny:
+				arr2 = np.zeros((arr.shape[0], ny - arry, 3))
 				arr = np.concatenate((arr, arr2), axis=1)
 			arr = arr.astype('float32')/255.0
 			X = []
@@ -40,7 +46,7 @@ for i in range(df.size):
 			X = np.asarray(X)
 			X = X.astype('float32')/255
 			result = fcnmodel.predict(X)[0]
-			predicted_class = result[:,:,1]
+			predicted_class = np.argmax(result, axis=2)
 			for y_in in range(0, ny, 32):
 				for x_in in range(0, nx, 32):
 					flag = False
@@ -51,7 +57,16 @@ for i in range(df.size):
 								flag = True
 								break
 						if flag == False:
-							not_wildebeest = not_wildebeest.append({'image_name':filename, 'xcoord':x_t, 'ycoord':y_t}, ignore_index=True)
+							filenames.append(filename)
+							x_ts.append(x_t)
+							y_ts.append(y_t)
+							# not_wildebeest = not_wildebeest.append({'image_name':filename, 'xcoord':x_t, 'ycoord':y_t}, ignore_index=True)
+			# predicted_class = predicated_class * 255.0
+			# predicted_class = np.concatenate(np.concatenate(predicted_class, predicted_class, axis=2), predicted_class, axis=2)
+			# imarr[x_pos:x_pos+arrx,y_pos:arry+y_pos),:] = predicted_class[0:arrx,0:arry, :]
+	# img = Image.fromarray(np.uint8(im_arr))
+	# img.save('2015/test/' + str(filename) + '.png')
+	not_wildebeest = pd.DataFrame(np.column_stack([filenames,x_ts,y_ts]), columns=['image_name', 'xcoord', 'ycoord'])
+	not_wildebeest.to_csv('new_not_wildebeest_locations.csv', header=False, mode='a')
 
-print('Finished. Saving non-wildebeest co-ordinates.')
-not_wildebeest.to_csv('new_not_wildebeest_locations.csv')
+print('Finished.')
--- train_vgg16_iterate.py
+++ train_vgg16_iterate.py
@@ -1,53 +1,91 @@
 import time,os,sys
-from PIL import Image
+# from PIL import Image
 import numpy as np
 import cv2
 import pandas as pd
-from keras.utils.np_utils import to_categorical
-from deepModels import *
+# from keras.utils.np_utils import to_categorical
+# from deepModels import *
 from sklearn.utils import shuffle
 
 nx = 512
 ny = 512
 
-fcnmodel = getVgg16SegModel(ny,nx)
+# fcnmodel = getVgg16SegModel(ny,nx)
 num_classes=2
 
-fcnmodel.load_weights('vgg16-header_classifier.h5')
+# fcnmodel.load_weights('vgg16-header_classifier.h5')
 not_wildebeest_locs = pd.read_csv('new_not_wildebeest_locations.csv')
 not_wildebeest_locs = shuffle(not_wildebeest_locs)
+print('No of false positives in unfiltered list: ' + str(not_wildebeest_locs.size))
+xcounter, ycounter = 0,0
+print(pd.unique(not_wildebeest_locs.ycoord.values))
+'''
+print(not_wildebeest_locs[(not_wildebeest_locs.xcoord >= 32 * (((512 * xcounter) // 32) + 2))
+                                                & (not_wildebeest_locs.xcoord <= min(7296, 32 * (((512 * (xcounter + 1)) // 32) - 2)))
+                                                & (not_wildebeest_locs.ycoord >= 32 * (((512 * ycounter) // 32) + 2))
+                                                & (not_wildebeest_locs.ycoord <= min(4848, 32 * (((512 * (ycounter + 1)) // 32) - 2)))
+                                                ].head(50))
+'''
+# set up folders
+fold_num = 0
+while True:
+    if not(os.path.exists('2015/cls_train_images/nw/' + str(fold_num))):
+        os.makedirs('2015/cls_train_images/nw/' + str(fold_num))
+        break
+    fold_num = fold_num + 1
+image_store = '2015/cls_train_images/nw/' + str(fold_num) + '/'
+print('Folder created: ' + image_store)
 
-print(fcn_model.summary())
+# print(fcn_model.summary())
 
-fcn_model.compile(optimizer='rmsprop',loss='binary_crossentropy',metrics=['accuracy'])
+# fcn_model.compile(optimizer='rmsprop',loss='binary_crossentropy',metrics=['accuracy'])
 
 #create training samples
 train_images = []
 train_labels = []
 
-import glob
+# import glob
 
-for filename in glob.iglob('2015/cls_train_images/w/*.jpg'):
-    img = cv2.imread(filename)
-    if img is not None:
-        train_images.append(np.array(img))
-        train_labels.append(1)
+# for filename in glob.iglob('2015/cls_train_images/w/*.jpg'):
+#     img = cv2.imread(filename)
+#     if img is not None:
+#         train_images.append(np.array(img))
+#         train_labels.append(1)
 
-# use equal non-wildebeest images for training
-not_wildebeest_locs = not_wildebeest_locs[:len(train_labels)]
-not_wildebeest_images = not_wildebeest_locs['image_name'].drop_duplicates()
+# throw out anything which might have been within two 32 pixel squares from the edge of a 512x512 tile (or the image edge)
+not_wildebeest_locs_true = not_wildebeest_locs.head(0)
+for xcounter in range((7360 // 512) + 1):
+    for ycounter in range((4912 // 512) + 1):
+#         print('Adding false positives with x >= ' + str(32 * (((512 * xcounter) // 32) + 2)) + ' and x <= ' + str(min(7296, 32 * (((512 * (xcounter + 1)) // 32) - 2)))
+#             + ' and y >= ' + str(32 * (((512 * ycounter) // 32) + 2)) + ' and y <= ' + str(min(4848, 32 * (((512 * (ycounter + 1)) // 32) - 2))))
+        not_wildebeest_locs_true.append(not_wildebeest_locs[(not_wildebeest_locs.xcoord >= 32 * (((512 * xcounter) // 32) + 2))
+                                                & (not_wildebeest_locs.xcoord <= min(7296, 32 * (((512 * (xcounter + 1)) // 32) - 2)))
+                                                & (not_wildebeest_locs.ycoord >= 32 * (((512 * ycounter) // 32) + 2))
+                                                & (not_wildebeest_locs.ycoord <= min(4848, 32 * (((512 * (ycounter + 1)) // 32) - 2)))])
+not_wildebeest_locs = not_wildebeest_locs_true
+print('No of false positives in filtered list: ' + str(not_wildebeest_locs.size))
+
+# use equal non-wildebeest images for training; temporarily removed to write out all images
+# not_wildebeest_locs = not_wildebeest_locs[:len(train_labels)]
+
+not_wildebeest_images = pd.DataFrame(not_wildebeest_locs['image_name'].drop_duplicates())
+
 for i1, image_row in not_wildebeest_images.iterrows():
+    imfound = 0
     not_wildebeest_sub_locs = not_wildebeest_locs[not_wildebeest_locs.image_name == image_row.image_name]
     img = cv2.imread('2015/' + image_row.image_name + '.JPG')
     # pad images at edges with 0s
     for i2, loc in not_wildebeest_sub_locs.iterrows():
         img2 = np.array(img)[max(0, loc.ycoord - 32):min(4912, loc.ycoord + 32),max(0, loc.xcoord - 32):min(7360, loc.xcoord + 32),:]
+        assert img2.shape == (64, 64, 3)
         pad_img = np.zeros((64, 64, 3))
         x_offset, y_offset = max(0, 32 - loc.xcoord), max(0, 32 - loc.ycoord)
         pad_img[y_offset:y_offset + img2.shape[0], x_offset:x_offset + img2.shape[1], :] = img2
+        cv2.imwrite(image_store + image_row[0] + '_' + str(imfound) + '.jpg', pad_img)
         train_images.append(pad_img)
         train_labels.append(0)
 
+'''
 print(len(train_images))
 print(len(train_labels))
 
@@ -93,3 +131,4 @@ print('Training ends')
 
 fcn_model.save_weights('vgg16-header_classifier.h5')
 print('Total time taken:' + str(end - start))
+'''
